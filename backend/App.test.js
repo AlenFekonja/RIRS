120200b86e080ea7c308ff1019371f3cf653da2a
Alen Fekonja <alen.fekonja@student.um.si>
2024-12-14 12:38:39 +0100

    test4

--- backend/App.test.js
+++ backend/App.test.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv');
 const mysql = require('mysql2');
 
 dotenv.config();
-//test3
+//test4
 test('should have all required environment variables defined', () => {
     const requiredEnvVars = [
         'DB_HOST',
